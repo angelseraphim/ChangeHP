3fef776d8833b534c792285c51f61045ce85a518
AngelSeraphim <AngelSeraphim@DESKTOP-J0TQGTE>
2024-07-16 16:52:27 +0500

    Добавьте файлы проекта.

---
+++ ChangeHP/Config.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Interfaces;
+using PlayerRoles;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ChangeHP
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        [Description("Role and HP")]
+        public Dictionary<RoleTypeId, int> HPVal { get; set; } = new Dictionary<RoleTypeId, int>()
+        {
+            {RoleTypeId.Scp939, 3000},
+            {RoleTypeId.NtfCaptain, 120}
+        };
+    }
+}
---
+++ ChangeHP/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+
+namespace ChangeHP
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Prefix => "ChangeHP";
+        public override string Name => "ChangeHP";
+        public override string Author => "angelseraphim.";
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.ChangingRole += OnChangingRole;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.ChangingRole -= OnChangingRole;
+            base.OnDisabled();
+        }
+        private void OnChangingRole(ChangingRoleEventArgs ev)
+        {
+            if (Config.HPVal.ContainsKey(ev.NewRole))
+            {
+                ev.Player.MaxHealth = Config.HPVal[ev.NewRole];
+                ev.Player.Health = Config.HPVal[ev.NewRole];
+            }
+        }
+    }
+}
